12f8b739f60ab33971e7e47e59f478137e589d91
Ahmed A Youssef <se.ahmed.ashraf.1705@gmail.com>
2018-12-03 22:04:41 +0200

    Login with some changes
    none

--- Etrafe3ly/src/pages/home/home.ts
+++ Etrafe3ly/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { person } from '../../modules/person.interface';
+import { lawyer } from '../../modules/lawyer.class';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private afAuth:AngularFireAuth , private toastCTRL:ToastController,
+    private navParams:NavParams) {
+      if(this.navParams.data){
+        this.subscription = navParams;
+      }
+    
   }
-
+  //attributes
+  subscription;
+  userData = {};
+  ionViewDidLoad() {
+    console.log(this.navParams.data.uid);
+    this.subscription = this.afAuth.authState.subscribe( (data) =>{
+      if(data && data.email && data.uid){
+        this.userData = data;
+        this.toastCTRL.create({
+          message : `welcome  ${data.email},`,
+          duration : 3000
+        }).present();
+      }
+        else {
+          this.toastCTRL.create({
+            message : `This account is not registered`,
+            duration : 3000
+          }).present();
+        }
+    });
+    
+    }
+    
+  
+  logoutUser(){
+    this.subscription.unsubscribe();
+    this.navParams.data = {};
+  }
+  
 }
